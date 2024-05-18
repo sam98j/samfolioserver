59e749cdbfa0b403afb5d2f6060777760af97a6d
HosamAlden <hosam98j@gmail.com>
2024-05-18 07:52:06 +0200

    adding src folder

--- src/app.ts
+++ src/app.ts
@@ -16,14 +16,14 @@ app.use(cors());
 // load awilix (IOC) container
 loadContainer(app);
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(loadControllers('controllers/*.ts', { cwd: __dirname }));
 // catch 404 and forward to error handler
 app.use(function (req, res) {
